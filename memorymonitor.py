8bd447921812e0c3ebf8efcbd666536a5c44b6ba
bcarpenter480 <bcarpenter480@gmail.com>
2023-12-07 16:40:47 +0000

    Add memory leak detection algorithm and documentation

--- memorymonitor.py
+++ memorymonitor.py
@@ -7,6 +7,9 @@ import datetime
 import numpy as np
 import psutil as ps
 import threading
+from typing import List, Tuple
+
+import scipy
 
 try:
     import ccs
@@ -16,6 +19,16 @@ except ImportError:
     
 
 class MemorySnapper:
+    """Environment process memory information recorder
+    
+    Example usage::
+        >>> mem_snap = MemorySnapper() #Create a memory snapper object
+        >>> mem_snap.take_memory_snapshot() #Take a snapshot of the current memory usage
+        >>> <Do some stuff that should leave the system with the same memory as before>
+        >>> mem_snap.take_memory_snapshot() #Take another snapshot
+        >>> assert mem_snap.detect_leaks() == set() #No memory leaks detected
+    
+    """
 
     class ProcMemData:
         """Class to store memory data for a single process"""
@@ -35,11 +48,13 @@ class MemorySnapper:
             self._vmss[time] = full_memory.vms
 
         @property
-        def vmss(self):
+        def vmss(self) ->List[int]:
+            """Returns a List[int] of virtual memory sizes for a process over time"""
             return list(self._vmss.values())
 
         @property
-        def times(self):
+        def times(self)->List[datetime.datetime]:
+            """Returns a List[datetime.datetime] of times at which a memory snapshot was taken"""
             return list(self._vmss.keys())
 
     def __init__(self, existing_data_file=None, base_monitoring=False):
@@ -62,6 +77,7 @@ class MemorySnapper:
             self.__data = {}
 
     def proc_by_name(self, name):
+        """Return a Dict[datetime.datetime, int] of process data by the name of that process"""
         for pid in self.pids:
             if self.__data[pid].name == name:
                 return self.__data[pid]
@@ -69,29 +85,35 @@ class MemorySnapper:
     def __getitem__(self, pid):
         return self.__data[pid]
 
-    @property
-    def processes(self):
-        return self.__proc_names
-
-    @property
-    def pids(self):
-        return self.__data.keys()
-
     def __enter__(self):
         return self
 
+    @property
+    def processes(self)->List[str]:
+        """List of processes names for which memory data has been collected"""
+        return self.__proc_names
+
+    @property
+    def pids(self)->List[int]:
+        """List of processes ids for which memory data has been collected"""
+        return self.__data.keys()
+
     def close(self):
+        """Close the memory monitoring object, saving the collected data as a pickle"""
         with open(self.__data_file, "wb") as fp:
             pickle.dump(self.__dict__, fp)
 
     def take_memory_snapshot(self):
+        """Create an entry in the data structure for memory processes in the environment at the
+        current time.
+        """
 
         # SETUP TIME
         if CCSENV:
             total = 0
             env_procs = ccs.GetEnvProcs(
                 full_report=True
-            )  # Whilst process_iter might be as thread safe as can be this definetly isn't
+            )  # Whilst process_iter might thread safe this ccs.GetEnvProcs is not
             del env_procs[ccs.procName]
             env_pids = {v["pid"]: k for k, v in env_procs.items()}
 
@@ -118,23 +140,41 @@ class MemorySnapper:
             # Update memory usage
                 self[p_pid][current_time] = p.memory_info()
             # total_mem = total_mem + self[p_pid][current_time]
-        print(f"Total memory usage: {total_mem}")
+        logging.info(f"Total memory usage: {total_mem}")
         self.totals[current_time]=total_mem
 
-    def detect_leaks(self,algo="linefit"):
+    def detect_leaks(self,algo="linefit")->Tuple[List[str],List[int]]:
         """ Detect memory leaks using a given algorithm
-        
+
         Args:
             algo: Algorithm to use to detect memory leaks
-        
-        Returns: A set of process names that are abnormally using memory
+
+        Returns:
+            A set of names and pids of processes that are abnormally using memory
         """
         if algo=="linefit":
-            return self.detect_leaks_line_fit()
+            abnorm_names, abnorm_pids =  self.detect_leaks_line_fit()
         else:
             raise NotImplementedError()
+        abnorm_names = list(abnorm_names)
+        abnorm_pids = list(abnorm_pids)
 
-    def detect_leaks_line_fit(self):
+        for proc_index in range(len(abnorm_names)):
+            logging.warning(f"Abnormal memory usage detected in process: {abnorm_names[proc_index]}"
+                            f"with pid {abnorm_pids[proc_index]}")
+            print(f"Abnormal memory usage detected in process: {abnorm_names[proc_index]} with pid "
+                  f"{abnorm_pids[proc_index]}")
+
+        return (abnorm_names, abnorm_pids)
+
+    def detect_leaks_line_fit(self)->Tuple[List[str],List[int]]:
+        """
+        Fit a line to the avalible memory data, assuming a 'nice' fit and if it has a particuarly 
+        large gradient then suggest it as a memory leaking process.
+
+        Returns:
+            A set of names and pids of processes that are abnormally using memory
+        """
         grads = []
         abnorm_names = set()
         abnorm_pids = set()
@@ -144,10 +184,47 @@ class MemorySnapper:
                 abnorm_names.add(self.__data[proc].name)
                 abnorm_pids.add(proc)
             grads.append(m) 
-        logging.warning(type((abnorm_names, abnorm_pids)))
         return (abnorm_names, abnorm_pids)
 
+    def detect_leaks_linear_backward_regression(self)->Tuple[List[str],List[int]]:
+        """Detect memory leaks using the linear backward regression algorithm
+        Not yet implemented sucessfully
+        """
+        return NotImplementedError("This method is not implemented")
+        for pid in self.pids:
+            input_data = self[pid]
+            anomalus_ts = set()#type :set(datetimes) #Anomalus data points
+            anomalus_names = set()
+            anomalus_pids = set()
+            WINDOW_MIN = 4 # Add in some form of smoothing
+            WINDOW_MAX = len(input_data.times)
+            R_sqr_min = 0.8 #From paper
+            CRITICAL_TIME_MAX = None
+            CRITICAL_MEMORY_USAGE = ps.virtual_memory().total
+            i = WINDOW_MIN
+            n = len(input_data.times)
+            while(i<= n and i<=WINDOW_MAX):
+                ts = input_data.times[n-i:n]
+                ys = input_data.vmss[n-i:n]
+                m,c,r_pcc,*_ =scipy.stats.linregress(ts,ys, full=True) #Gives us Pearson correlation coeff unlike np.polyfit
+                r2 = r_pcc**2 #Rsquare is the square of the pearson correlation coefficient
+                t_crit = (CRITICAL_MEMORY_USAGE - c)/m
+                if (r2>=R_sqr_min and t_crit < CRITICAL_TIME_MAX):
+                    #Add ts to the erronus stamps (unpack)
+                    # anomalus_ts = anomalus_ts | set(ts[n-i:i]) #Add new ts to set of anomalus ones
+                    anomalus_names.add(self.__data[pid].name)
+                    anomalus_pids.add(pid)
+
+                i = i+1
+            return anomalus_ts
+
     def plot_data(self, proc_pid=None):
+        """
+        Plot the memory usage of a process over time or all processes if proc_pid is None
+        
+        Args:
+            proc_pid: Process id of process to plot default is None which plots all processes
+        """
         if proc_pid is None:
             procs_to_plot = self.pids
 
@@ -155,7 +232,7 @@ class MemorySnapper:
             procs_to_plot = [proc_pid]
 
         for proc in procs_to_plot:
-            plt.scatter(self[proc].times, np.array(self[proc].vmss) / 1e6, label=self.__data[proc].name)
+            plt.scatter(self[proc].times, np.array(self[proc].vmss) / 1e6, label=(self.__data[proc].name,proc))
 
         plt.legend()
         plt.xlabel("Time stamp")
@@ -165,6 +242,7 @@ class MemorySnapper:
         plt.show()
 
 class MemoryMonitor(MemorySnapper):
+    """Class for continuous monitoring of processes memory usage """
     def __init__(self, time_interval=1, base_monitoring=True):
         super().__init__(existing_data_file=None, base_monitoring=base_monitoring)
 
