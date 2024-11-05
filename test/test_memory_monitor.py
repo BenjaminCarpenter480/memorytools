a4e53b72fef25f3c0b413fd3d9f6ab9514f56e7f
Benjamin Carpenter <bcarpenter480@gmail.com>
2024-11-05 18:31:02 +0000

    Update PORT for test server

--- test/test_memory_monitor.py
+++ test/test_memory_monitor.py
@@ -27,7 +27,7 @@ def start_server(request: type[pytest.FixtureRequest]): #Do not change api
     """
     
     global PORT
-    PORT = 8128
+    PORT = 8130
 
     #Spawn a test_server.py with Popen, yield and then kill it
     proc = subprocess.Popen(["python3", "test/test_server.py"])
